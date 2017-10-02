b8af70c8422e10adb20fbc7a0a9cd704de239693
1amageek <norikazu-muramoto@cookpad.com>
2017-10-02 13:52:57 +0900

    first commit

---
+++ Injectable/Injectable.h
@@ -0,0 +1,19 @@
+//
+//  Injectable.h
+//  Injectable
+//
+//  Created by nori on 2017/10/02.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Injectable.
+FOUNDATION_EXPORT double InjectableVersionNumber;
+
+//! Project version string for Injectable.
+FOUNDATION_EXPORT const unsigned char InjectableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Injectable/PublicHeader.h>
+
+
---
+++ Injectable/Injectable.swift
@@ -0,0 +1,20 @@
+//
+//  Injectable.swift
+//  Injectable
+//
+//  Created by nori on 2017/10/02.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol Injectable {
+    associatedtype Dependency = Void
+    func inject(_ dependency: Dependency)
+}
+
+public extension Injectable where Dependency == Void {
+    func inject(_ dependency: Dependency) {
+
+    }
+}
